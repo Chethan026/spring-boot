eb2c1762b4f3b56afb5cb1ff824cab8d5310c263
chethan kumar DL <32458393+Chethan026@users.noreply.github.com>
2021-03-09 16:41:09 +0530

    Add files via upload

---
+++ restful-web-services/src/main/java/com/chethan/webServices/rest/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,24 @@
+package com.chethan.webServices.rest.restfulwebservices;
+
+public class HelloWorldBean {
+	
+	String message;
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+}
